c60a6df1a43d7c7ad189d467d86792dfca096919
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-05-10 00:46:03 +0200

    added attachement

--- notify.py
+++ notify.py
@@ -1,23 +1,29 @@
 import sendgrid
+import os
+from sendgrid.helpers.mail import *
+import base64
 
 class emailNotifier():
     def __init__(self, sender, recipient):
         """
         Constructor to the emailNotifier class, takes sender and recipient email addresses
         """
-        self.sender = sender
-        self.recipient = recipient
-        self.client = sendgrid.SendGridClient("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")
-        self.message = sendgrid.Mail()
+        self.sender = Email(sender)
+        self.recipient = Email(recipient)
+        self.sg = sendgrid.SendGridAPIClient(apikey = "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")
 
-    def sendEmail(self, subject, body):
+    def sendEmail(self, subject, body, attachementPath, attachementName):
         """
         a function that takes the subject and the body as input and sends an email
         """
         self.subject = subject
-        self.body = body
-        self.message.add_to(self.recipient)
-        self.message.set_from(self.sender)
-        self.message.set_subject(self.subject)
-        self.message.set_html(self.body)
-        self.client.send(self.message)
+        self.body = Content("text/plain", body)
+        # content = Content("text/plain", "and easy to do anywhere, even with Python")
+        with open(attache+39/mentPath, 'rb') as fd:
+             b64data = base64.b64encode(fd.read())
+        attachment = Attachment()
+        attachment.content = str(b64data,'utf-8')
+        attachment.filename = attachementName
+        self.message = Mail(self.sender, self.subject, self.recipient, self.body)
+        self.message.add_attachment(attachment)
+        response = self.sg.client.mail.send.post(request_body=self.message.get())
