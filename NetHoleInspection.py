6eca00737550c1106cfbf5a2a5c9678b308e596c
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-05-11 03:12:32 +0200

    fishnet holes model prediction

--- NetHoleInspection.py
+++ NetHoleInspection.py
@@ -4,8 +4,25 @@ from cognitiveServices import Server
 class NetHoleDetection(Server):
     def __init__(self):
         self.httpAddress = 'australiaeast.api.cognitive.microsoft.com'
-        self.subAddress = "/customvision/v3.0/Prediction/0894325a-97a6-4390-9277-3ecda4230665/classify/iterations/Cars%20or%20Trucks/image"
+        self.subAddress = "/customvision/v3.0/Prediction/0081ddad-4c13-4705-ac6b-819068f863f8/classify/iterations/Fishnet_Holes_Detection/image"
         self.predKey = 'bd5db334ffa249f1bd8ca2d43ccf5da3'
         self.contentType = 'application/octet-stream'
         self.predkey = '/subscriptions/16e8c7d2-4877-4c8f-bb77-ae2931dd33eb/resourceGroups/Test/providers/Microsoft.CognitiveServices/accounts/Test_Prediction'
         super().__init__(self.httpAddress, self.subAddress, self.predKey, self.contentType, self.predkey)
+
+    def predict(self, imgPath):
+        """
+        a function that returns:
+            * 1 if the fishnet doesn't have a hole
+            * 2 if the fishnet has a hole
+            * 0 otherwise
+        """
+        pred = self.getPrediction(imgPath).decode()
+        print(pred)
+        tag_index = pred.find("tagId") + 8
+        if(pred[tag_index:tag_index+1] == 'f'):
+            return 1
+        elif(pred[tag_index:tag_index+1] == '4'):
+            return 2
+        else:
+            return 0
