8130b63160107d0e5e96690caa7e59b15600471f
abdullahsalah96 <abdullahsalah96@outlook.com>
2019-05-21 18:24:24 +0200

    added fishnet cleaning

--- NetCleaningInspection.py
+++ NetCleaningInspection.py
@@ -5,7 +5,7 @@ class NetCleaning(Server):
     def __init__(self):
         #change subAddress
         self.httpAddress = 'australiaeast.api.cognitive.microsoft.com'
-        self.subAddress = "/customvision/v3.0/Prediction/0894325a-97a6-4390-9277-3ecda4230665/classify/iterations/Cars%20or%20Trucks/image"
+        self.subAddress = "/customvision/v3.0/Prediction/8803f1fd-5690-4ed9-8a95-6097586fcf46/classify/iterations/Iteration1/image"
         self.predKey = 'bd5db334ffa249f1bd8ca2d43ccf5da3'
         self.contentType = 'application/octet-stream'
         self.predkey = '/subscriptions/16e8c7d2-4877-4c8f-bb77-ae2931dd33eb/resourceGroups/Test/providers/Microsoft.CognitiveServices/accounts/Test_Prediction'
